5381838ee36bb604e1cb33e431a57a42ca179e8c
zainabmirza-eng <zainabmirzafatah99@gmail.com>
2024-03-16 22:03:18 +0300

    added

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"}
+  ]
+
+
+  function superbowlWin(arrayOfObjects) {
+    const win = arrayOfObjects.find(game => game.result === 'W');
+    
+    return win ? win.year : undefined;
+
+}
